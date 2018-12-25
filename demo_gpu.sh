e001f62f78a7b5bb8e4357837ddb8c3539f9057b
Mingfei Ma <mingfei.ma@intel.com>
2018-12-25 15:41:47 +0800

    modify gpu run script

--- demo_gpu.sh
+++ demo_gpu.sh
@@ -2,4 +2,4 @@ ARGS=""
 if [ "$1" == "prof" ]; then
     ARGS="$ARGS -prof"
 fi
-python train.py -data data/demo -save_model demo-model -train_steps 150 $ARGS -gpuid 0
+python train.py -data data/demo -save_model demo-model -train_steps 150 $ARGS -gpuid 0 -world_size 1 -gpu_ranks 0
